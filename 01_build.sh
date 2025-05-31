db16bde4a09c259930c9ed0a2157c30be8218026
Noah <science103555@gmail.com>
2025-06-01 01:29:32 +0800

    [v1.7.0.0] flow optimization

--- 01_build.sh
+++ 01_build.sh
@@ -11,17 +11,21 @@ cmake -S $HOME/project/traccc -B $HOME/project/traccc/build \
 
 cmake --build $HOME/project/traccc/build -- -j$(nprocs) 2>&1 | tee ./log/build02.log
 
-grep -P "ptxas info.*Used" ./log/build02.log | \
-  sed -e 's/^.*ptxas info/ptxas info/' | sort -u | tee ./log/ptxas.log
-
-# 列出所有有 spill 的行
-grep -E "bytes spill (stores|loads)" ./log/build02.log | tee ./log/spill_summary.log
-
-# 若要看完整上下文（包含該 kernel 名稱、register count）
-grep -nE "ptxas info.*Function properties" -n ./log/build02.log \
-  | cut -d: -f1 | while read L; do
-      sed -n "$((L-2)),$((L+3))p" ./log/build02.log \
-        | grep -E "spill|Used [0-9]+ registers" && echo
-  done | tee ./log/spill.log
-
 grep -rn error -A 2 ./log/build02.log | tee ./log/error.log
+
+./02_run.sh
+
+#   grep -P "ptxas info.*Used" ./log/build02.log | \
+#     sed -e 's/^.*ptxas info/ptxas info/' | sort -u | tee ./log/ptxas.log
+#   
+#   # 列出所有有 spill 的行
+#   grep -E "bytes spill (stores|loads)" ./log/build02.log | tee ./log/spill_summary.log
+#   
+#   # 若要看完整上下文（包含該 kernel 名稱、register count）
+#   grep -nE "ptxas info.*Function properties" -n ./log/build02.log \
+#     | cut -d: -f1 | while read L; do
+#         sed -n "$((L-2)),$((L+3))p" ./log/build02.log \
+#           | grep -E "spill|Used [0-9]+ registers" && echo
+#     done | tee ./log/spill.log
+
+
--- 05_diff_baseline.sh
+++
@@ -1,1 +0,0 @@
-git diff 4ca46ed0 -- . ':(exclude)*.log'   ':(exclude)*.sh'   ':(exclude)*.ncu-rep'   ':(exclude)*.csv' ':(exclude)./03_nc_report' ':(exclude)./.gitignore' | tee ./log/gitdiff.log
