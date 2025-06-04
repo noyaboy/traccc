5762e0e291c1e2607926291e64926b89d711c489
Hao-Jyun Liang <99811508+noyaboy@users.noreply.github.com>
2025-06-05 02:16:51 +0800

    fix wmma multiply shared memory

--- device/cuda/include/traccc/cuda/utils/wmma_matrix_multiply.hpp
+++ device/cuda/include/traccc/cuda/utils/wmma_matrix_multiply.hpp
@@ -23,9 +23,9 @@ __device__ inline detray::dmatrix<algebra_t, M, N> wmma_multiply(
     const detray::dmatrix<algebra_t, K, N>& B) {
     constexpr int TILE = 16;
 
-    __shared__ __align__(16) half Ah[TILE * TILE];
-    __shared__ __align__(16) half Bh[TILE * TILE];
-    __shared__ __align__(16) float Ch[TILE * TILE];
+    __align__(16) half Ah[TILE * TILE];
+    __align__(16) half Bh[TILE * TILE];
+    __align__(16) float Ch[TILE * TILE];
 
 
     for (int idx = 0; idx < TILE * TILE; ++idx) {
