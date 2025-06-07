8acf4f5dbd8a7e2f0f0ea89479bc50d793481f29
Hao-Jyun Liang <99811508+noyaboy@users.noreply.github.com>
2025-06-07 17:24:25 +0800

    Fix constant config declaration

---
+++ device/cuda/src/seeding/kernel_config.cu
@@ -0,0 +1,14 @@
+#define TRACCC_DEFINE_SEEDING_CONFIG
+#include "kernel_config.cuh"
+
+namespace traccc::cuda::kernels {
+
+void load_seeding_config(const seedfinder_config& finder_cfg,
+                         const seedfilter_config& filter_cfg) {
+    cudaMemcpyToSymbol(g_seedfinder_cfg_bytes, &finder_cfg,
+                       sizeof(seedfinder_config));
+    cudaMemcpyToSymbol(g_seedfilter_cfg, &filter_cfg,
+                       sizeof(seedfilter_config));
+}
+
+}  // namespace traccc::cuda::kernels
